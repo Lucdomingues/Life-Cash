8bc9c5ddb2b91f13766ba6e6161ded082438b25e
Lucas Camargo <lojalucc@outlook.com>
2026-02-24 12:43:53 -0300

    feat: modelDB insetLogs

--- src/db/LogsDB.js
+++ src/db/LogsDB.js
@@ -2,8 +2,8 @@ import connection from "./connection.js";
 
 const insertLogs = async (event, person_id) => {
   await connection.execute(
-    `INSERT INTO logs (event, timestamp, person_id) VALUES (?, ?, ?)`,
-    [event, new Date(), person_id],
+    `INSERT INTO logs (event, timestamp, person_id) VALUES (?, CURRENT_TIMESTAMP, ?)`,
+    [event, person_id],
   );
 };
 
